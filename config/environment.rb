1290ba9143f998c261a9cf4e7bd3f5155944fd50
Erik Hellman <erik.hellman@me.com>
2012-07-06 18:33:20 +0200

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Keepit140::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keepit140::Application.config.secret_token = '32a0db9b2e89633fbb0e6fa33995fa07c7eeacb4528990101a1ecf04aff47b15febcedbbcdedd1956d32953b54d08ce53c6f417ff982b74bb737245f764f0145'
