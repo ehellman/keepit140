5f267e6f30633dfdad9391e6de709091ff3fc871
Erik Hellman <erik.hellman@me.com>
2012-08-25 02:53:45 +0200

    Finish updating users

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Erik Hellman"
-    email    "erik@example.com"
-    password "12rikeri"
-    password_confirmation "12rikeri"
+    sequence(:name)  { |n| "Person #{n}"}
+    sequence(:email) { |n| "persona_#{n}@example.com"}
+    password "passwd"
+    password_confirmation "passwd"
+
+    factory :admin do
+      admin true
+    end
   end
 end
